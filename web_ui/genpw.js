9f9c1973d2d963e53a112a98784adb7ac984e7c4
Ray Morris <deepmagicbeginshere@gmail.com>
2022-07-12 19:44:46 -0700

    genpw.js: Add local scoring of years and sequential digits

--- web_ui/genpw.js
+++ web_ui/genpw.js
@@ -1,9 +1,38 @@
+function scoreDigits(password) {
+  if (/^\d+$/.test(newpassword)) {
+    return;
+  }
+
+  var i, years;
+
+  if ( "01234567890".includes(password) || "09876543210".includes(password) ) {
+    return 2;
+  }
+
+  years = set();
+  i = 1940;
+
+  while (i < 2030) {
+    years.add(i.toString());
+    i += 1;
+  }
+
+  if ( years.has(password) ) {
+    return 2;
+  }
+
+  return password.length;
+}
 
 
 function scoreLocal() {
     // TODO consider doing more scoring locally
     // 1.97 is log10(95), the number of possible characters
-    meter.value = ( Math.round(newpassword.value.length * 1.97) )
+    if (/^\d+$/.test(newpassword.value)) {
+        meter.value = scoreDigits(newpassword.value);
+    } else {
+        meter.value = ( Math.round(newpassword.value.length * 1.97) );
+    }
 }
 
 function scorePassword() {
@@ -21,7 +50,8 @@ function scorePassword() {
         meter.value = jsonResponse.score
     }
   };
-  xhttp.open("GET", "http://localhost:8080/?newpassword=" + encodeURIComponent(newpassword.value), true);
+  // xhttp.open("GET", "http://localhost:8080/?newpassword=" + encodeURIComponent(newpassword.value), true);
+  xhttp.open("GET", "/cgi-bin/password-dog/password-dog.cgi?newpassword=" + encodeURIComponent(newpassword.value), true);
   xhttp.send();
 }
 /*
